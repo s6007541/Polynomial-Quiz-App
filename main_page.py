eadb78c3cb719299645447a7ff26dc771953e802
Sorn Chottananurak <31609767+s6007541@users.noreply.github.com>
2022-06-28 10:35:40 +0900

    Add files via upload

--- main_page.py
+++ main_page.py
@@ -7,6 +7,7 @@ st.markdown("""
     This Website was created by Sorn Chottananurak for my Brothers to practice for an exam.
          """)
 st.sidebar.markdown("# Main page 🤯")
+st.markdown("![Alt Text](https://images.squarespace-cdn.com/content/v1/5e5d5b916fe1d748eabefec6/1585608460341-WWG02ZAE8JCKYDL5VEBA/KOBE.gif?format=750w)")
 
 
 
